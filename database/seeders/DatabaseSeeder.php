68801c13d408bc5911b85dc4023705df7c963798
mail21 <ismailidn1@gmail.com>
2021-07-18 16:18:45 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('admins')->insert([
+            'username' => 'root',
+            'nama' => 'Kamera',
+            'password' => 'root',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
